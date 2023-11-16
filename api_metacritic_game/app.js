d6a160d6dd8262a271bef55942819fe6a1098c61
theoMANEVIT31 <151016675+theoMANEVIT31@users.noreply.github.com>
2023-11-16 16:22:35 +0100

    Add files via upload

--- api_metacritic_game/app.js
+++ api_metacritic_game/app.js
@@ -1,25 +1,50 @@
+// Déclaration des modules à importer
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const OpenApiValidator = require('express-openapi-validator')
 const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
 
-const swaggerDocument = YAML.load('./open_api.yaml')
+// Déclaration du swagger document pour être servi en statique
+const swaggerDocument = YAML.load('./openapi.yaml')
 
-app.use(express.json())
+// Utilisation de middleware globaux
+app.use(cors()); // Autorise toutes les requêtes de tout origine
+app.use(express.json()); // Permet de parser automatiquement le json en entrée
 
+// Middleware d'openAPI
 app.use(
     OpenApiValidator.middleware({
-        apiSpec: './open_api.yaml',
+        apiSpec: './openapi.yaml',
         ignoreUndocumented: true
     })
 )
 
+// Déclaration des routers principaux qui utilisent les sous-routers
+const criticsEditorsRouter = require('./routers/criticsEditors')
+app.use('/criticsEditors', criticsEditorsRouter);
+
+const criticsUsersRouter = require('./routers/criticsUsers')
+app.use('/criticsUsers', criticsUsersRouter);
+
+const editorsRouter = require('./routers/editors')
+app.use('/editors', editorsRouter);
+
+const reviewRouter = require('./routers/reviews')
+app.use('/reviews', reviewRouter)
+
+const loginRouter = require('./routers/login')
+app.use('/login', loginRouter)
+
+// Par défaut quand on appellera "/" on veut servir en statique la doc OpenAPI
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
+// Déclaration globale du middleware d'erreur, on assume que le paramètre error, possède certains attributs
 app.use((error, req, res, next) => {
     res.status(error.status || 500)
-        .json({success: false, messag: error.message, status: error.status})
+        .json({success: false, message: error.message, status: error.status})
 })
 
+// On oublie pas d'exporter pour tester
 module.exports = app
---
+++ api_metacritic_game/db.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    host: "localhost",
+    username: "root",
+    password: "root",
+    database: "metaCritic",
+    port: 3306
+}
+
