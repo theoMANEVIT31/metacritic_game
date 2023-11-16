4c4f128a4e34a47d8b7af04f4c38fa27f7722b0f
theoMANEVIT31 <151016675+theoMANEVIT31@users.noreply.github.com>
2023-11-16 16:23:10 +0100

    Add files via upload

--- api_metacritic_game/server.js
+++ api_metacritic_game/server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const db = require('./models/index');
+const app = require('./app');
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000...')
-})
+db.instance.sync({force: true}).then(async () => {
+    console.log('Database connected and synchronized');
+
+    //await db.books.create({title: "Default Book", date: "2022-12-05"});
+
+    app.listen(3000, () => {
+        console.log('Server running on port 3000 !');
+    });
+}).catch((e) => {
+    console.error(e);
+});
