fb0458cc87a63cff85708ddb4ebe89f799a03631
theoMANEVIT31 <151016675+theoMANEVIT31@users.noreply.github.com>
2023-11-23 14:15:17 +0100

    Repare POST

--- api_metacritic_game/server.js
+++ api_metacritic_game/server.js
@@ -4,7 +4,15 @@ const app = require('./app');
 db.instance.sync({force: true}).then(async () => {
     console.log('Database connected and synchronized');
 
-    await db.users.create({idU: 1, name: "test"});
+    await db.users.create({idU: 1, name: "user1"});
+
+    await db.editors.create({idE: 1, name: "editor1"});
+
+    await db.criticsEditors.create({idU: 1, description: "criticeditor1", idE: 1});
+
+    await db.reviews.create({idR: 1, noteE: 10, noteU: 10, description: "description1", titre:"titre1", date:"23/11/2023", idC:1});
+
+    await db.criticsUsers.create({idR: 1, idU: 1, description: "description1"});
 
     app.listen(3000, () => {
         console.log('Server running on port 3000 !');
--- api_metacritic_game/services/reviewsService.js
+++ api_metacritic_game/services/reviewsService.js
@@ -4,8 +4,8 @@ exports.getReviews = async () => {
     return await db.reviews.findAll();
 }
 
-exports.addReview = (idR, noteE, noteU, description, titre, date) => {
-    return db.reviews.create({idR, noteE, noteU, description, titre, date});
+exports.addReview = (idR, noteE, noteU, description, titre, date, idC) => {
+    return db.reviews.create({idR, noteE, noteU, description, titre, date, idC});
 }
 
 exports.getReviewById = async (idR) => {
