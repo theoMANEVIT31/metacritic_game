606df8b437ec76ecab3f9c1269509cb8525a6411
Sarouman54 <sarahbarrabe@gmail.com>
2023-11-30 23:19:37 +0100

    addRegexRules

--- api_metacritic_game/controllers/registerController.js
+++ api_metacritic_game/controllers/registerController.js
@@ -1,35 +1,49 @@
 var bcrypt  = require('bcrypt')
 // var jwt     = require('jsonwebtoken')
 const db    = require('../models/indexModel')
+const regexEmail = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/
+const regexPseudo = /^[\w\-]+$/
 
+exports.signUp = async (req, res) => {
+  const email   = req.body.email
+  const password= req.body.password
+  const name    = req.body.name
 
-exports.signUp = (req, res) => {
-    bcrypt.hash(req.body.password, 10)  // 10 = saltRound
-      .then(hash => {
-        // console.log(new users())
-        db.users.create({
-            name: req.body.name,
-            email: req.body.email,
-            hashedPassword: req.body.password
-        })
-        // const user = new users({
-        //     // idU: 1,
-        //     name: req.body.name,
-        //     email: req.body.email,
-        //     hashedPassword: req.body.password
-        // })
-        // console.log(user)
-        // user.save()
-          .then(() => res.status(201).json({
-            message: 'User created !'
-        }))
-          .catch(error => res.status(400).json({
-            error: 'An error occured during user\'s creation !'
-        }))
+  if(!email || !password || !name) return res.status(400).json({
+      success: false,
+      message: 'Name, email and password are required'
+    })
+  if(await db.users.findOne({where: {email: email}}))return res.status(400).json({
+      success: false,
+      message: 'This user already have an account'
+    })
+  if(!regexEmail.test(email)) return res.status(400).json({
+      success: false,
+      message: 'Email not valid'
+    })
+  if(!regexPseudo.test(name)) return res.status(400).json({
+      success: false,
+      message: 'Name can contains only alphanumeric character and \'_\' and \'-\''
+    })
+  
+
+  bcrypt.hash(password, 10)  // 10 = saltRound
+    .then(hash => {
+      db.users.create({
+          name: name,
+          email: email,
+          hashedPassword: hash
       })
-      .catch(error => res.status(500).json({
-        error: 'An error occured during hashing password !'
+        .then(() => res.status(201).json({
+          message: 'User created !'
       }))
+        .catch(error => res.status(400).json({
+          error: 'An error occured during user\'s creation !'
+      }))
+    })
+    .catch(error => res.status(500).json({
+      error: 'An error occured during hashing password !'
+    }))
 }
 
 exports.signIn = (req, res) => {
