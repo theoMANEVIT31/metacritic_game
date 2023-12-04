f5bf5d7f2698ce18dfcf557094cbf8602faea081
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2023-12-04 14:22:44 +0100

    Update infosJeu.js

--- api_metacritic_game/services/infosJeu.js
+++ api_metacritic_game/services/infosJeu.js
@@ -1,6 +1,7 @@
-const configApiExterne = require('config.api.externe.js');
+const configApiExterne = require('./../config.api.externe.js');
+const axios = require('axios');
 
-exports.getInfosJeu = async () => {
+exports.getAllNomJeu = async () => {
     await configApiExterne.getAuthorization();
     fetch(
         "https://api.igdb.com/v4/games",
@@ -10,7 +11,7 @@ exports.getInfosJeu = async () => {
             'Client-ID': configApiExterne.client_id,
             'Authorization': `Bearer ${configApiExterne.access_token}`
           },
-          body: "name, storyline, summary, alternative_name, category, cover, release_dates, franchise, genres, involved_companies, keywords, platforms, status;"
+          body: "name;"
         }
     ) .then(response => {
             console.log(response.json());
@@ -19,4 +20,30 @@ exports.getInfosJeu = async () => {
             console.error(err);
         }
     );
+}
+
+
+exports.getInfosJeuByName = async (nameJeu) => {
+  await configApiExterne.getAuthorization();
+
+  let data = `fields name, storyline, summary, alternative_name, category, cover, release_dates, franchise, genres, involved_companies, keywords, platforms, status;\r\nwhere name = ${nameJeu};`;
+
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://api.igdb.com/v4/games',
+    headers: { 
+      'Client-ID': configApiExterne.client_id,
+      'Authorization': `Bearer ${configApiExterne.access_token}`, 
+      'Content-Type': 'text/plain'
+    },
+    data
+  };
+  axios.request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 }
