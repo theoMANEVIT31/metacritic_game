36181360d9c7ba033334e7d532c46299503c35b8
theoMANEVIT31 <151016675+theoMANEVIT31@users.noreply.github.com>
2023-12-11 20:41:06 +0100

    v5

--- api_metacritic_game/server.js
+++ api_metacritic_game/server.js
@@ -10,9 +10,9 @@ db.instance.sync({force: true}).then(async () => {
 
     await db.criticsEditors.create({evaluation: "bien",noteE: 8, idE: 1});
 
-    await db.reviews.create({avgU: 9.8, title: "007 Legends: Skyfall", idC: 1});
+    await db.reviews.create({avgU: 9.8, description: "Take on the role of James Bond as played by Daniel Craig in all new SKYFALL levels. Unlock three new single player levels following the storyline of the SKYFALL movie. Levels take place in Istanbul and Shanghai as the player (Bond) attempts to track down ",title: "007 Legends: Skyfall", release: "2012/11/20",idC: 1});
 
-    //await db.criticsUsers.create({idR: 1, idU: 1, comment: "Super jeu", noteU: 10});
+    await db.criticsUsers.create({idR: 1, idU: 1, comment: "Super jeu", noteU: 10});
 
     app.listen(3000, () => {
         console.log('Server running on port 3000 !');
--- api_metacritic_game/services/reviewsService.js
+++ api_metacritic_game/services/reviewsService.js
@@ -5,22 +5,40 @@ exports.getReviews = async () => {
   return await db.reviews.findAll();
 };
 
-
 exports.addReview = async (avgU, title, idC) => {
   try {
-    const gameInfo = await igdbService.getInfosJeuByName(title); 
+    // Utilisez title comme il est s'il est déjà correctement formaté
+    const gameInfo = await igdbService.getInfosJeuByName(title);
 
-    const { first_release_date, summary} = gameInfo[0]; 
+    // Vérifier si gameInfo est défini et s'il contient des éléments
+    if (gameInfo && gameInfo.length > 0) {
+      const { first_release_date, summary, name } = gameInfo[0];
 
-    const review = await db.reviews.create({
-      avgU,
-      description: summary,
-      title,
-      release: first_release_date,
-      idC,
-    });
+      // Convertir le timestamp en une date JavaScript
+      const releaseDate = new Date(first_release_date * 1000); // Notez la multiplication par 1000 pour convertir secondes en millisecondes
 
-    return review; 
+      // Obtenir les composants de la date
+      const year = releaseDate.getFullYear();
+      const month = String(releaseDate.getMonth() + 1).padStart(2, '0'); // Les mois commencent à 0
+      const day = String(releaseDate.getDate()).padStart(2, '0');
+
+      // Format de la date
+      const formattedDate = `${year}/${month}/${day}`;
+
+      const review = await db.reviews.create({
+        avgU,
+        description: summary,
+        title: name, // Utiliser le nom du jeu du JSON
+        release: formattedDate,
+        idC,
+      });
+
+      return review;
+    } else {
+      // Log pour débuguer
+      console.log('Game information not found:', title);
+      throw new Error('Game information not found.');
+    }
   } catch (error) {
     console.error(error);
     throw new Error('Failed to add review');
