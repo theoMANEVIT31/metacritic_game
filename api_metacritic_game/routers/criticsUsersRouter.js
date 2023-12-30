084d8d9a4fc24575111ae5ef127ac32705d8a3cf
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2023-12-30 13:49:24 +0100

    Update criticsUsersRouter.js

--- api_metacritic_game/routers/criticsUsersRouter.js
+++ api_metacritic_game/routers/criticsUsersRouter.js
@@ -1,14 +1,14 @@
 const express = require('express'),
     router = express.Router(),
     criticsUsersController = require('../controllers/criticsUsersController'),
-    authMiddleware = require('../middlewares/authMiddleware');
+    authMiddleware = require('../middlewares/authMiddleware')
 
 
-router.get('/', criticsUsersController.getCriticsUsers);
-router.post('/', authMiddleware, criticsUsersController.addCriticsUser);
-router.put('/', authMiddleware, criticsUsersController.putCriticsUser);
-router.get('/:idU/:idR', criticsUsersController.getCriticsUserById);
-router.delete('/:idU/:idR', authMiddleware, criticsUsersController.deleteCriticsUserById);
+router.get('/', criticsUsersController.getCriticsUsers)
+router.post('/', authMiddleware, criticsUsersController.addCriticsUser)
+router.put('/', authMiddleware, criticsUsersController.putCriticsUser)
+router.get('/:idU/:idR', criticsUsersController.getCriticsUserById)
+router.delete('/:idU/:idR', authMiddleware, criticsUsersController.deleteCriticsUserById)
 
 
-module.exports = router;
+module.exports = router
