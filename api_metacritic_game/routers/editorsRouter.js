341dce037414635e1f6954efe9d8ef9da74d76d2
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2023-12-30 13:49:37 +0100

    Update editorsRouter.js

--- api_metacritic_game/routers/editorsRouter.js
+++ api_metacritic_game/routers/editorsRouter.js
@@ -1,14 +1,14 @@
 const express = require('express'),
     router = express.Router(),
     editorsController = require('../controllers/editorsController'),
-    authMiddleware = require('../middlewares/authMiddleware');
+    authMiddleware = require('../middlewares/authMiddleware')
 
 
-router.get('/', editorsController.getEditors);
-router.post('/', authMiddleware, editorsController.addEditor);
-router.put('/', authMiddleware, editorsController.putEditor);
-router.get('/:idE', editorsController.getEditorById);
-router.delete('/:idE', authMiddleware, editorsController.deleteEditorById);
+router.get('/', editorsController.getEditors)
+router.post('/', authMiddleware, editorsController.addEditor)
+router.put('/', authMiddleware, editorsController.putEditor)
+router.get('/:idE', editorsController.getEditorById)
+router.delete('/:idE', authMiddleware, editorsController.deleteEditorById)
 
 
-module.exports = router;
+module.exports = router
