978775c3748525cd82dc64937f9e2d8a02fa614a
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2023-12-30 13:50:36 +0100

    Update usersRouter.js

--- api_metacritic_game/routers/usersRouter.js
+++ api_metacritic_game/routers/usersRouter.js
@@ -1,14 +1,14 @@
 const express = require('express'),
     router = express.Router(),
     usersController = require('../controllers/usersController'),
-    authMiddleware = require('../middlewares/authMiddleware');
+    authMiddleware = require('../middlewares/authMiddleware')
 
 
-router.get('/', usersController.getUsers);
-router.post('/', authMiddleware, usersController.addUser);
-router.put('/', authMiddleware, usersController.putUser);
-router.get('/:idU', usersController.getUserById);
-router.delete('/:idU', authMiddleware, usersController.deleteUserById);
+router.get('/', usersController.getUsers)
+router.post('/', authMiddleware, usersController.addUser)
+router.put('/', authMiddleware, usersController.putUser)
+router.get('/:idU', usersController.getUserById)
+router.delete('/:idU', authMiddleware, usersController.deleteUserById)
 
 
-module.exports = router;
+module.exports = router
