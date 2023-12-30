3aaff9a1a135f9e2e22d0f345f405810f5080c11
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2023-12-30 21:12:40 +0100

    Update criticsEditorsRouter.js

--- api_metacritic_game/routers/criticsEditorsRouter.js
+++ api_metacritic_game/routers/criticsEditorsRouter.js
@@ -4,11 +4,11 @@ const express = require('express'),
     authMiddleware = require('../middlewares/authMiddleware')
 
 
-router.get('/', criticsEditorsController.getCriticsEditors); // tous le monde peut accéder aux critiques des "editor" sur les reviews
-router.post('/', authMiddleware("editor"), criticsEditorsController.addCriticsEditor); // seul les "editor" peuvent poster des critiques editeur sur les reviews
-router.put('/', authMiddleware("editor"), criticsEditorsController.putCriticsEditor); // seul les "editor" peuvent modifier leur critique
-router.get('/:id', criticsEditorsController.getCriticsEditorById); // tous le monde peut accéder à une critique d'"editor" en particulier
-router.delete('/:id', authMiddleware("editor"), criticsEditorsController.deleteCriticsEditorById); // seul les utilisateurs ayant pour rôle "editor" peuvent supprimer leurs critiques
+router.get('/', criticsEditorsController.getCriticsEditors) // tous le monde peut accéder aux critiques des "editor" sur les reviews
+router.post('/', authMiddleware("editor"), criticsEditorsController.addCriticsEditor) // seul les "editor" peuvent poster des critiques editeur sur les reviews
+router.put('/', authMiddleware("editor"), criticsEditorsController.putCriticsEditor) // seul les "editor" peuvent modifier leur critique
+router.get('/:id', criticsEditorsController.getCriticsEditorById) // tous le monde peut accéder à une critique d'"editor" en particulier
+router.delete('/:id', authMiddleware("editor"), criticsEditorsController.deleteCriticsEditorById) // seul les utilisateurs ayant pour rôle "editor" peuvent supprimer leurs critiques
 
 
 module.exports = router
