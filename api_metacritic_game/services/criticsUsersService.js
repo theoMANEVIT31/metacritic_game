6607fcfeb63c28af5b6022532c56d997e11b5ce9
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2023-12-30 13:51:55 +0100

    Update criticsUsersService.js

--- api_metacritic_game/services/criticsUsersService.js
+++ api_metacritic_game/services/criticsUsersService.js
@@ -1,31 +1,31 @@
-const db = require('../models/indexModel');
+const db = require('../models/indexModel')
 
 
 exports.getCriticsUsers = async () => {
-  return await db.criticsUsers.findAll();
-};
+  return await db.criticsUsers.findAll()
+}
 
 exports.addCriticsUser = async (idR, idU, comment, noteU) => {
   return await db.criticsUsers.create(
     { idR, idU, comment, noteU }
-  );
-};
+  )
+}
 
 exports.getCriticsUserById = async (idR, idU) => {
   return await db.criticsUsers.findOne(
     { where: { idR, idU } }
-  );
-};
+  )
+}
 
 exports.putCriticsUser = async (idR, idU, comment, noteU) => {
   return await db.criticsUsers.update(
     { comment: comment, noteU: noteU },
     { where: { idR, idU } }
-  );
-};
+  )
+}
 
 exports.deleteCriticsUserById = async (idR, idU) => {
   return await db.criticsUsers.destroy(
     { where: { idR, idU } }
-  );
-}; 
+  )
+}
