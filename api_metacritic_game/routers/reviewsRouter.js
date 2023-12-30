a72c391199cee366872eed6338cebfb4a769d2e4
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2023-12-30 13:50:12 +0100

    Update reviewsRouter.js

--- api_metacritic_game/routers/reviewsRouter.js
+++ api_metacritic_game/routers/reviewsRouter.js
@@ -1,14 +1,14 @@
 const express = require('express'),
     router = express.Router(),
     reviewsController = require('../controllers/reviewsController'),
-    authMiddleware = require('../middlewares/authMiddleware');
+    authMiddleware = require('../middlewares/authMiddleware')
 
     
-router.get('/', reviewsController.getReviews);
-router.post('/', authMiddleware, reviewsController.addReview);
-router.put('/', authMiddleware, reviewsController.putReview);
-router.get('/:idR', reviewsController.getReviewById);
-router.delete('/:idR', authMiddleware, reviewsController.deleteReviewById);
+router.get('/', reviewsController.getReviews)
+router.post('/', authMiddleware, reviewsController.addReview)
+router.put('/', authMiddleware, reviewsController.putReview)
+router.get('/:idR', reviewsController.getReviewById)
+router.delete('/:idR', authMiddleware, reviewsController.deleteReviewById)
 
 
-module.exports = router;
+module.exports = router
