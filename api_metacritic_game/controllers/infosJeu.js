e79cb55ed14ac9e3046d626a9c5fa850d0849730
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2023-12-30 13:44:39 +0100

    Update infosJeu.js

--- api_metacritic_game/controllers/infosJeu.js
+++ api_metacritic_game/controllers/infosJeu.js
@@ -1,13 +1,13 @@
-const infosJeu = require('../services/infosJeu');
+const infosJeu = require('../services/infosJeu')
 
 
 exports.getAllNomJeu = (req, res) => {
-    const game = infosJeu.getAllNomJeu();
-    res.json({ data: game});
+    const game = infosJeu.getAllNomJeu()
+    res.json({ data: game})
 };
 
 exports.getInfosJeuByName = (req, res) => {
-  const game = infosJeu.getInfosJeuByName();
-  res.json({ data: game});
+  const game = infosJeu.getInfosJeuByName()
+  res.json({ data: game})
 };
        
