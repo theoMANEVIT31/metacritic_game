a2ad01b39a1af39b98d394b16bc9f0246dba4ab1
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2023-12-30 13:49:08 +0100

    Update criticsEditorsRouter.js

--- api_metacritic_game/routers/criticsEditorsRouter.js
+++ api_metacritic_game/routers/criticsEditorsRouter.js
@@ -1,14 +1,14 @@
 const express = require('express'),
     router = express.Router(),
     criticsEditorsController = require('../controllers/criticsEditorsController'),
-    authMiddleware = require('../middlewares/authMiddleware');
+    authMiddleware = require('../middlewares/authMiddleware')
 
 
-router.get('/', criticsEditorsController.getCriticsEditors);
-router.post('/', authMiddleware, criticsEditorsController.addCriticsEditor);
-router.put('/', authMiddleware, criticsEditorsController.putCriticsEditor);
-router.get('/:idC', criticsEditorsController.getCriticsEditorById);
-router.delete('/:idC', authMiddleware, criticsEditorsController.deleteCriticsEditorById);
+router.get('/', criticsEditorsController.getCriticsEditors)
+router.post('/', authMiddleware, criticsEditorsController.addCriticsEditor)
+router.put('/', authMiddleware, criticsEditorsController.putCriticsEditor)
+router.get('/:idC', criticsEditorsController.getCriticsEditorById)
+router.delete('/:idC', authMiddleware, criticsEditorsController.deleteCriticsEditorById)
 
 
-module.exports = router;
+module.exports = router
