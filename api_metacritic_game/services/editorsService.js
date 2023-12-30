0d53d9ca99a2ba271df570ac1e5a4c95cac9e908
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2023-12-30 13:52:16 +0100

    Update editorsService.js

--- api_metacritic_game/services/editorsService.js
+++ api_metacritic_game/services/editorsService.js
@@ -1,33 +1,33 @@
-const db = require("../models/indexModel");
+const db = require("../models/indexModel")
 
 
 exports.getEditors = async () => {
   return await db.editors.findAll(
     { attributes: { exclude: ["hashedPassword"] } }
-  );
-};
+  )
+}
 
 exports.addEditor = async (pseudo, hashedPassword, email) => {
   return await db.editors.create(
     { pseudo, hashedPassword, email }
-  );
-};
+  )
+}
 
 exports.getEditorById = async (idE) => {
   return await db.editors.findOne(
     { where: { idE }, attributes: { exclude: ["hashedPassword"] }, }
-  );
-};
+  )
+}
 
 exports.putEditor = async (idE, pseudo, email, hashedPassword) => {
   return await db.editors.update(
     { pseudo: pseudo, email: email, hashedPassword: hashedPassword },
     { where: { idE } }
-  );
-};
+  )
+}
 
 exports.deleteEditorById = async (idE) => {
   return await db.editors.destroy({
     where: { idE }
-  });
-};
+  })
+}
