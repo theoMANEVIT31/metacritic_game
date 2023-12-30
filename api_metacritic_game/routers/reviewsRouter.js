751101967811fe307fc17d68affd35028c738e2f
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2023-12-30 21:13:16 +0100

    Update reviewsRouter.js

--- api_metacritic_game/routers/reviewsRouter.js
+++ api_metacritic_game/routers/reviewsRouter.js
@@ -3,11 +3,11 @@ const express = require('express'),
     reviewsController = require('../controllers/reviewsController'),
     authMiddleware = require('../middlewares/authMiddleware')
 
-router.get('/', reviewsController.getReviews); // tout le monde peut accéder à l'ensemble des review
-router.post('/', authMiddleware("editor"), reviewsController.addReview); // seul les utilisateurs avec le rôle "editor" peuvent ajouter une nouvelle review
-router.put('/', authMiddleware("editor"), reviewsController.putReview); // seul les utilisateurs avec le rôle "editor" peuvent modifier les reviews
-router.get('/:id', reviewsController.getReviewById); // tout le monde peut accéder à une review particulière
-router.delete('/:id', authMiddleware("editor"), reviewsController.deleteReviewById); // seul les utilisateurs avec le rôle "editor" peuvent supprimer les reviews
+router.get('/', reviewsController.getReviews) // tout le monde peut accéder à l'ensemble des review
+router.post('/', authMiddleware("editor"), reviewsController.addReview) // seul les utilisateurs avec le rôle "editor" peuvent ajouter une nouvelle review
+router.put('/', authMiddleware("editor"), reviewsController.putReview) // seul les utilisateurs avec le rôle "editor" peuvent modifier les reviews
+router.get('/:id', reviewsController.getReviewById) // tout le monde peut accéder à une review particulière
+router.delete('/:id', authMiddleware("editor"), reviewsController.deleteReviewById) // seul les utilisateurs avec le rôle "editor" peuvent supprimer les reviews
 
 
 module.exports = router
