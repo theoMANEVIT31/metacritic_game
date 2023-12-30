dea64f8b7fdc91328b02b24ae8bca939f22ed5ae
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2023-12-30 13:52:58 +0100

    Update reviewsService.js

--- api_metacritic_game/services/reviewsService.js
+++ api_metacritic_game/services/reviewsService.js
@@ -1,22 +1,22 @@
-const db = require("../models/indexModel");
-const igdbService = require('./infosJeu');
+const db = require("../models/indexModel")
+const igdbService = require('./infosJeu')
 
 
 exports.getReviews = async () => {
   return await db.reviews.findAll();
-};
+}
 
 exports.addReview = async (avgU, title, idC) => {
   try {
-    const gameInfo = await igdbService.getInfosJeuByName(title);
+    const gameInfo = await igdbService.getInfosJeuByName(title)
 
     if (gameInfo && gameInfo.length > 0) {
-      const { first_release_date, summary, name } = gameInfo[0];
-      const releaseDate = new Date(first_release_date * 1000);
-      const year = releaseDate.getFullYear();
-      const month = String(releaseDate.getMonth() + 1).padStart(2, '0');
-      const day = String(releaseDate.getDate()).padStart(2, '0');
-      const formattedDate = `${year}/${month}/${day}`;
+      const { first_release_date, summary, name } = gameInfo[0]
+      const releaseDate = new Date(first_release_date * 1000)
+      const year = releaseDate.getFullYear()
+      const month = String(releaseDate.getMonth() + 1).padStart(2, '0')
+      const day = String(releaseDate.getDate()).padStart(2, '0')
+      const formattedDate = `${year}/${month}/${day}`
 
       const review = await db.reviews.create({
         avgU,
@@ -24,28 +24,28 @@ exports.addReview = async (avgU, title, idC) => {
         title: name,
         release: formattedDate,
         idC,
-      });
+      })
       return review;
     } else {
-      throw new Error('Game information not found.');
+      throw new Error('Game information not found.')
     }
   } catch (error) {
-    throw new Error('Failed to add review');
+    throw new Error('Failed to add review')
   }
-};
+}
 
 exports.getReviewById = async (idR) => {
   return await db.reviews.findOne(
     { where: { idR } }
-  );
-};
+  )
+}
 
 exports.putReview = async (idR, avgU, description, title, release, idC) => {
   return await db.reviews.update(
     { avgU: avgU, description: description, title: title, release: release, idC: idC },
     { where: { idR } }
-  );
-};
+  )
+}
 
 exports.deleteReviewById = async (idR) => {
   try {
@@ -55,14 +55,14 @@ exports.deleteReviewById = async (idR) => {
     if (hasRelatedCriticsUsers) {
       await db.criticsUsers.destroy(
         { where: { idR } }
-      );
+      )
     }
     return await db.reviews.destroy(
       { where: { idR } }
-    );
+    )
   } catch (error) {
     throw new Error(
       `Une erreur s'est produite lors de la suppression de l'avis : ${error}`
-    );
+    )
   }
-};
+}
