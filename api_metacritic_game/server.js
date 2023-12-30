d350693d828614b242d2e441705cc16514f64fba
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2023-12-30 21:19:11 +0100

    Update server.js

--- api_metacritic_game/server.js
+++ api_metacritic_game/server.js
@@ -1,13 +1,13 @@
-const db = require('./models/indexModel');
-const app = require('./app');
-const bcrypt = require('bcrypt');
-const { getIdRolesByNom } = require('./services/rolesService');
+const db = require('./models/indexModel')
+const app = require('./app')
+const bcrypt = require('bcrypt')
+const { getIdRolesByNom } = require('./services/rolesService')
 
 db.instance.sync({force: true}).then(async () => {
     console.log('Database connected and synchronized')
     app.listen(3000, () => {
-        console.log('Server running on port 3000 !');
-    });
+        console.log('Server running on port 3000 !')
+    })
 
     await db.roles.create({nom: "admin"});
     await db.roles.create({nom: "editor"});
@@ -35,11 +35,11 @@ db.instance.sync({force: true}).then(async () => {
         headers: {'Content-Type': 'application/json'}
     })
 
-    await db.criticsEditors.create({evaluation: "bien", note: 8, idEditor: 1});
+    await db.criticsEditors.create({evaluation: "bien", note: 8, idEditor: 1})
 
-    await db.reviews.create({avg: 9.8, description: "Take on the role of James Bond as played by Daniel Craig in all new SKYFALL levels. Unlock three new single player levels following the storyline of the SKYFALL movie. Levels take place in Istanbul and Shanghai as the player (Bond) attempts to track down ",title: "007 Legends: Skyfall", release: "2012/11/20",idCriticEditor: 1});
+    await db.reviews.create({avg: 9.8, description: "Take on the role of James Bond as played by Daniel Craig in all new SKYFALL levels. Unlock three new single player levels following the storyline of the SKYFALL movie. Levels take place in Istanbul and Shanghai as the player (Bond) attempts to track down ",title: "007 Legends: Skyfall", release: "2012/11/20",idCriticEditor: 1})
 
-    await db.criticsUsers.create({idReview: 1, idUser: 3, comment: "Super jeu", note: 10});
+    await db.criticsUsers.create({idReview: 1, idUser: 3, comment: "Super jeu", note: 10})
 
 
 }).catch((e) => {
